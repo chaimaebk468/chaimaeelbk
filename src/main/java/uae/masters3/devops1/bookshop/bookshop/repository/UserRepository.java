2fe658936919bd3226b5d5ccd28fa7eb4cb039b2
chaimaebk468 <chaimaeelbekkari@gmail.com>
2026-02-21 17:21:51 +0000

    Sauvegarde des modifications locales avant la fusion avec une autre branche

--- src/main/java/uae/masters3/devops1/bookshop/bookshop/repository/UserRepository.java
+++ src/main/java/uae/masters3/devops1/bookshop/bookshop/repository/UserRepository.java
@@ -1,3 +1,11 @@
+package uae.masters3.devops1.bookshop.bookshop.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uae.masters3.devops1.bookshop.bookshop.entity.User;
+
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
