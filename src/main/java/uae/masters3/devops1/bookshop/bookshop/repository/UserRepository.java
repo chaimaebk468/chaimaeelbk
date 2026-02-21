70b11d5d147821972894808d27dac28f083b7b69
hikmatbenaisa <hikmat.benaisa@etu.uae.ac.ma>
2026-02-21 17:34:58 +0100

    Modification de UserRepository et l'ajout du package Secrity avec ses Classes

--- src/main/java/uae/masters3/devops1/bookshop/bookshop/repository/UserRepository.java
+++ src/main/java/uae/masters3/devops1/bookshop/bookshop/repository/UserRepository.java
@@ -1,6 +1,10 @@
-package uae.masters3.devops.bookshop.api.repository;
+package uae.masters3.devops1.bookshop.bookshop.repository;
 
-import uae.masters3.devops.bookshop.api.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uae.masters3.devops1.bookshop.bookshop.entity.User;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
