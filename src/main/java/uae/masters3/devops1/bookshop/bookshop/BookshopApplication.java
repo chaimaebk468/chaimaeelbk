4dddd714709aa714d7f36b2c0640e6d305502451
chaimaebk468 <chaimaeelbekkari@gmail.com>
2026-02-21 13:05:35 +0000

    init: structure projet Spring Boot

---
+++ src/main/java/uae/masters3/devops1/bookshop/bookshop/BookshopApplication.java
@@ -0,0 +1,13 @@
+package uae.masters3.devops1.bookshop.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopApplication.class, args);
+    }
+
+}
